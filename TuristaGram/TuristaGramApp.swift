1d4827c2927f7e8ff400423110eb37824a5f14b1
Diany <salolu2016@gmail.com>
2026-04-29 23:27:16 -0500

    Initial Commit

---
+++ TuristaGram/TuristaGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuristaGramApp.swift
+//  TuristaGram
+//
+//  Created by user286595 on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct TuristaGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TuristaGramTests/TuristaGramTests.swift
@@ -0,0 +1,17 @@
+//
+//  TuristaGramTests.swift
+//  TuristaGramTests
+//
+//  Created by user286595 on 4/29/26.
+//
+
+import Testing
+@testable import TuristaGram
+
+struct TuristaGramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
